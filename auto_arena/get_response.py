4457669b9479a558eca2667ac2b27a1ed2909cea
Alanzxd <112223122+Alanzxd@users.noreply.github.com>
2024-06-16 15:41:04 +0800

    Update get_response.py

--- auto_arena/get_response.py
+++ auto_arena/get_response.py
@@ -1,9 +1,9 @@
 import os
 import fire
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig
-from torch.nn.parallel import DistributedDataParallel 
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import openai
+from vllm import LLM
 
 from utils import existing_model_paths
 
@@ -14,34 +14,31 @@ def load_model(model_name):
         raise ValueError("Unsupported model")
 
     if model_info == "OPENAI":
-        # print("OpenAI model detected, calling API.")
-        return None, None  
+        return None, None, model_info
 
     if os.path.exists(model_info):
         print(f"HF model detected, loading from: {model_info}")
         tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=model_info, trust_remote_code=True)
         print("Tokenizer Loaded; Loading Model")
         model = AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path=model_info, device_map = 'auto', torch_dtype="auto")
-        return tokenizer, model
+        return tokenizer, model, model_info
+    
+    if model_info.startswith("VLLM"):
+        print(f"VLLM model detected, loading from: {model_info}")
+        model = LLM(model_info)
+        return None, model, model_info
 
     raise FileNotFoundError("Model path does not exist")
 
-def run_hf_model(prompts, tokenizer, model, temperature=0.7, max_tokens=1024):
+def run_hf_model(prompts, tokenizer, model):
     if not callable(tokenizer):
         raise TypeError("Tokenizer is not callable. Ensure it is correctly initialized.")
     tokenizer.pad_token = tokenizer.eos_token
     inputs = tokenizer(prompts, padding=True, return_tensors="pt").to("cuda:7")
 
-    outputs = model.generate(
-        **inputs,
-        temperature=temperature,
-        max_new_tokens=max_tokens,
-        do_sample= (temperature >= 1e-4)
-    )
+    # 调用model.generate，不传递任何额外参数
+    outputs = model.generate(**inputs)
 
-    # VLLM 
-
-    # Decoding all responses
     responses = []
     for i in range(outputs.shape[0]):
         full_response = tokenizer.decode(outputs[i], skip_special_tokens=True)
@@ -53,18 +50,17 @@ def run_hf_model(prompts, tokenizer, model, temperature=0.7, max_tokens=1024):
         responses.append(response)
 
     return responses
+
+def run_vllm_model(prompts, model):
+    outputs = model.generate(prompts)
     
-    # full_response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    responses = []
+    for output in outputs:
+        responses.append(output.text)
+    
+    return responses
 
-    # prompt_end_idx = full_response.find(prompt) + len(prompt)
-    # if prompt_end_idx > -1 and prompt_end_idx < len(full_response):
-    #     response = full_response[prompt_end_idx:].strip()
-    # else:
-    #     response = full_response
-
-    # return response
-
-def run_openai_model(prompts, model_name, client, temperature=0.7, max_tokens=1024):
+def run_openai_model(prompts, model_name, client):
     if "3.5-turbo-0125" in model_name: 
         model_name = "gpt-3.5-turbo-0125"
     elif "4-1106" in model_name: 
@@ -74,7 +70,6 @@ def run_openai_model(prompts, model_name, client, temperature=0.7, max_tokens=10
         completion = client.chat.completions.create(
         model=model_name,
         messages=[
-            # {"role": "system", "content": "You are an impartial evaluator."},
             {"role": "user", "content": prompt}
         ]
         )
@@ -82,13 +77,19 @@ def run_openai_model(prompts, model_name, client, temperature=0.7, max_tokens=10
         responses.append(str(text))
     return responses
 
-
-# def get_response(prompt, tokenizer, model, temperature=0.7, max_tokens=1024):
-    
-#     if tokenizer and model:
-#         response = run_hf_model(prompt, model, tokenizer, model, temperature, max_tokens)
-
-#     return str(response)
-
 if __name__ == "__main__":
+    def get_response(prompts, model_name):
+        tokenizer, model, model_info = load_model(model_name)
+
+        if model_name.startswith("VLLM"):
+            responses = run_vllm_model(prompts, model)
+        elif model_name == "OPENAI":
+            openai.api_key = os.getenv("OPENAI_API_KEY")
+            client = openai.OpenAI(api_key=openai.api_key)
+            responses = run_openai_model(prompts, model_name, client)
+        else:
+            responses = run_hf_model(prompts, tokenizer, model)
+        
+        return responses
+
     fire.Fire(get_response)
