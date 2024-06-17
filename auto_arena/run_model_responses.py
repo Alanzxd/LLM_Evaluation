a946216c20e835aaa5ede8d9c323983c7e6e6990
Alanzxd <112223122+Alanzxd@users.noreply.github.com>
2024-06-17 22:31:51 +0800

    Update run_model_responses.py

--- auto_arena/run_model_responses.py
+++ auto_arena/run_model_responses.py
@@ -20,8 +20,8 @@ def load_model(model_name):
 
     raise FileNotFoundError("Model path does not exist")
 
-def run_vllm_model(prompts, model, num_beams=1, max_new_tokens=200):
-    sampling_params = SamplingParams(num_beams=num_beams, max_tokens=max_new_tokens)
+def run_vllm_model(prompts, model, max_new_tokens=200):
+    sampling_params = SamplingParams(max_tokens=max_new_tokens)
     outputs = model.generate(prompts, sampling_params=sampling_params)
     
     responses = []
@@ -48,13 +48,13 @@ def save_responses(responses, model_name, output_dir, prompt_ids):
         for model, qid in empty_responses:
             print(f"Model: {model}, Question ID: {qid}")
 
-def get_responses(prompts, model_name, output_dir="model_responses", num_beams=1, max_new_tokens=200):
+def get_responses(prompts, model_name, output_dir="model_responses", max_new_tokens=200):
     if model_name in ["gpt4-1106", "gpt3.5-turbo-0125"]:
         print(f"Skipping model: {model_name}")
         return []
     else:
         model = load_model(model_name)
-        responses = run_vllm_model(prompts, model, num_beams, max_new_tokens)
+        responses = run_vllm_model(prompts, model, max_new_tokens)
 
     save_responses(responses, model_name, output_dir, list(range(len(prompts))))
     return responses
@@ -67,7 +67,7 @@ def get_questions():
     questions = load_jsonl("mt_bench_questions.jsonl")
     return [question['turns'][0] for question in questions]
 
-def run_all_models(model_names=None, output_dir="model_responses", num_beams=1, max_new_tokens=200, batch_size=1):
+def run_all_models(model_names=None, output_dir="model_responses", max_new_tokens=200, batch_size=1):
     prompts = get_questions()
     if model_names is None:
         model_names = list(existing_model_paths.keys())
@@ -79,7 +79,7 @@ def run_all_models(model_names=None, output_dir="model_responses", num_beams=1, 
     for model_name in tqdm(model_names):
         for i in range(0, len(prompts), batch_size):
             batch_prompts = prompts[i:i + batch_size]
-            get_responses(batch_prompts, model_name, output_dir, num_beams, max_new_tokens)
+            get_responses(batch_prompts, model_name, output_dir, max_new_tokens)
 
 if __name__ == "__main__":
     fire.Fire(run_all_models)
